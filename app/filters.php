4373d14465c84ab6dc1a4cbc708d44b65195a7eb
Jesus Carrero <jesusenrique.carrero@gmail.com>
2025-07-28 10:56:54 -0400

    Removing Wordpress Version from Head

--- app/filters.php
+++ app/filters.php
@@ -14,3 +14,20 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+/**
+ * Remove WordPress version information for security.
+ *
+ * @return void
+ */
+add_action('init', function () {
+    // Remove WordPress generator meta tag from head
+    remove_action('wp_head', 'wp_generator');
+});
+
+/**
+ * Remove generator info from RSS feeds.
+ *
+ * @return string
+ */
+add_filter('the_generator', '__return_empty_string');
